ec6ac5cf1aee34cef05ae1453417e58c9ab94f4a
Enda Whelan <endawhelan@hotmail.com>
2020-10-22 12:01:58 +0100

    react_init Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import './App.css';
+
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Change
         </p>
         <a
           className="App-link"
